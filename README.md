0871127863156d5cc3a826ae4103b70d9859faa1
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-25 14:37:08 +0200

    [add] We print posts from our feed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Social Posts
+- Creiamo il nostro array di oggetti post
+- Stampiamo i post del nostro feed
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Milestone 1: Creiamo il nostro array di oggetti post
 const posts = [
     {
         "id": 1,
@@ -55,3 +56,101 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// Milestone 2: Stampiamo i post del nostro feed
+const container = document.getElementById("container");
+
+// Funzione per creare un elemento HTML per rappresentare un post
+function createPostElement(post) {
+    // Creo un nuovo elemento div per rappresentare un post
+    const postElement = document.createElement("div");
+    postElement.className = "post";
+    
+    // Creo l'intestazione del post
+    const postHeader = document.createElement("div");
+    postHeader.className = "post__header";
+    
+    // Creo un elemento per contenere i metadati del post come autore e data
+    const postMeta = document.createElement("div");
+    postMeta.className = "post-meta";
+    
+    // Creo un elemento per l'icona del profilo dell'autore
+    const postMetaIcon = document.createElement("div");
+    postMetaIcon.className = "post-meta__icon";
+    
+    // Creo un elemento img per l'immagine del profilo dell'autore
+    const profilePic = document.createElement("img");
+    profilePic.className = "profile-pic";
+    profilePic.src = post.author.image;
+    profilePic.alt = post.author.name;
+    
+    // Collego l'immagine del profilo all'elemento dell'icona
+    postMetaIcon.appendChild(profilePic);
+    
+    // Creo un elemento per i metadati dell'autore e della data
+    const postMetaData = document.createElement("div");
+    postMetaData.className = "post-meta__data";
+    
+    // Creo un elemento per il nome dell'autore
+    const postMetaAuthor = document.createElement("div");
+    postMetaAuthor.className = "post-meta__author";
+    postMetaAuthor.textContent = post.author.name;
+    
+    // Creo un elemento per la data del post (es. "4 mesi fa")
+    const postMetaTime = document.createElement("div");
+    postMetaTime.className = "post-meta__time";
+
+    // Calcolo della data effettiva
+    postMetaTime.textContent = "4 mesi fa";
+    
+    // Collego il nome dell'autore e la data ai metadati
+    postMetaData.appendChild(postMetaAuthor);
+    postMetaData.appendChild(postMetaTime);
+    
+    // Collego l'icona del profilo e i metadati all'elemento dei metadati
+    postMeta.appendChild(postMetaIcon);
+    postMeta.appendChild(postMetaData);
+    
+    // Collego i metadati all'intestazione del post
+    postHeader.appendChild(postMeta);
+    
+    // Creo un elemento per il testo del post
+    const postText = document.createElement("div");
+    postText.className = "post__text";
+    postText.textContent = post.content;
+    
+    // Creo un elemento per l'immagine del post
+    const postImage = document.createElement("div");
+    postImage.className = "post__image";
+    
+    // Creo un elemento img per l'immagine del post
+    const img = document.createElement("img");
+    img.src = post.media;
+    img.alt = "";
+    
+    // Collego l'immagine del post all'elemento dell'immagine
+    postImage.appendChild(img);
+    
+    // Creo un elemento per il footer del post
+    const postFooter = document.createElement("div");
+    postFooter.className = "post__footer";
+
+    // Creo un elemento per il footer del post
+    const likes = document.createElement("div");
+    likes.className = "likes js-likes";
+
+    // Collego tutti gli elementi creati al post completo
+    postElement.appendChild(postHeader);
+    postElement.appendChild(postText);
+    postElement.appendChild(postImage);
+    postElement.appendChild(postFooter);
+
+    // Restituisci l'elemento del post completo
+    return postElement;
+}
+
+// Iteriamo attraverso l'array di post e creiamo gli elementi HTML per ciascun post
+posts.forEach((post) => {
+    const postElement = createPostElement(post);
+    container.appendChild(postElement);
+});
